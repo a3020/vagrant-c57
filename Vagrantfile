0327aff30a08fcf285a5f107eb1b017f56451120
a3020 <a3020@users.noreply.github.com>
2016-02-29 09:13:12 +0100

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,13 @@
+# This is ruby syntax. 2 is the Vagrant version
+Vagrant.configure(2) do |config|
+  config.vm.box = "ubuntu/trusty64"
+  
+  # This stuff is executed as root
+  config.vm.provision "shell", path: "bootstrap.sh"
+  config.vm.provision "file", source: "~/.gitconfig", destination: ".gitconfig"
+  
+  # http://127.0.0.1:8080
+  config.vm.network :forwarded_port, guest: 80, host: 8080, auto_correct: true
+  
+  config.vm.network "private_network", type: "dhcp"
+end
---
+++ bootstrap.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+cd /vagrant
+
+mkdir 000
+chown vagrant:vagrant 000
+
+apt-get update
+apt-get dist-upgrade
+
+
+echo "## Install Apache"
+# Assume yes, do not prompt
+apt-get install -y apache2
+
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
+
+# Enable mod_rewrite, allow .htaccess
+a2enmod rewrite
+
+# Allow overriding of the Apache config on a per directory basis.
+rm -f /etc/apache2/sites-enabled/000-default.conf
+cat > /etc/apache2/sites-enabled/000-default.conf <<EOL 
+<VirtualHost *:80>
+	ServerAdmin webmaster@localhost
+	DocumentRoot /var/www/000
+
+	<Directory /var/www/000>
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride All
+        Order allow,deny
+        allow from all
+	</Directory>
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+	CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOL
+
+
+echo "## Run Apache as vagrant user"
+sed -i 's/www-data/vagrant/g' /etc/apache2/envvars 
+
+
+echo "## Install PHP 5.6"
+add-apt-repository ppa:ondrej/php5-5.6
+sudo apt-get update
+apt-get install -y php5 php5-gd libapache2-mod-php5 php5-mysql php5-mcrypt php5-curl
+
+
+echo "## Install composer"
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin
+
+
+echo "## Install MySQL and create database (password is 'root')"
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+apt-get -q -y install mysql-server mysql-client
+
+mysql -u root -proot -e "CREATE DATABASE c57"
+
+
+echo "## Download C57"
+wget -q -O concrete5.base.zip https://www.concrete5.org/download_file/-/view/85780/
+
+
+echo "## Unpack C57"
+apt-get -y install unzip
+su vagrant -c 'unzip -q concrete5.base.zip'
+
+mv concrete5.*/* 000
+rm -rf concrete5.*/*
+rm concrete5.base.zip
+rmdir concrete5.*
+cd 000
+
+
+echo "## Restart Apache"
+/etc/init.d/apache2 restart
+
+
+echo "## Make PHP session path writable"
+chown vagrant:vagrant /var/lib/php5/sessions -R
+
+
+echo "## Install concrete5"
+chmod +x concrete/bin/concrete5
+concrete/bin/concrete5 c5:install --db-server=localhost --db-username=root --db-password=root --db-database=c57 \
+	--admin-email=admin@example.com --admin-password=admin \
+	--starting-point=elemental_blank
+
+cat > .htaccess <<EOL
+# -- concrete5 urls start --
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteBase /
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME}/index.html !-f
+RewriteCond %{REQUEST_FILENAME}/index.php !-f
+RewriteRule . index.php [L]
+</IfModule>
+# -- concrete5 urls end --
+EOL
+
+
+echo "## Done"
